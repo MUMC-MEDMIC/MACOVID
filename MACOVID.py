6132ebc9eb16eff8a6879565ae22c7d8ae646ce7
casperjamin <casperjamin@gmail.com>
2020-07-28 11:34:11 +0200

    wrapper

---
+++ MACOVID.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import yaml
+import os
+
+locationrepo = os.path.dirname(os.path.abspath(__file__)) 
+
+def get_absolute_path(path):
+    return os.path.abspath(path)
+
+def file_name_generator(filepath):
+    return os.path.splitext(os.path.basename(filepath))[0]
+
+def snakemake_in(samples, outdir):
+    samplesdic = {}
+    samplesdic['parameters'] = {}
+    samplesdic['parameters']["outdir"] = get_absolute_path(outdir)
+    samplesdic["SAMPLES"] = {}
+    
+    # generate the samples dictionary as input for snakemake 
+    for i in samples:
+        samplename = file_name_generator(i)
+        samplesdic["SAMPLES"][samplename] = get_absolute_path(i)
+    data = yaml.dump(samplesdic, default_flow_style=False)
+    
+    # make and write config file location
+    os.system(f"mkdir -p {locationrepo}/config")
+    with open(f"{locationrepo}/config/config.yaml", 'w') as f:
+        f.write(data)
+
+####################
+# Command line Parsers initialization
+####################
+
+def main(command_line = None):
+    #add main parser object
+    parser = ArgumentParser(description = "COVID seq toolkit...")
+
+    #add sub parser object
+    subparsers = parser.add_subparsers(dest = "mode")
+    #add snakemake pipeline to completely run fasta to clustered output
+    snakemake = subparsers.add_parser("mapreads", help = "run full pipeline from fastq to consensus sequence")
+    mapreads.add_argument("-i", required = True, dest = "input_files", nargs = "+")
+    mapreads.add_argument("--cores", dest = 'cores', required = True, type = int, help = 'Number of CPU cores to use')
+    mapreads..add_argument("-o", required = True, dest = "outdir")
+
+
+####################
+# parsing part
+####################
+    if args.mode == "mapreads":
+        snakemake_in(
+                samples = args.input_files,
+                outdir = args.outdir,
+                )
+        os.chdir(f"{locationrepo}")
+        os.system(f"snakemake --cores {args.cores} --use-conda")
+
+    else:
+        parser.print_usage()
+
+if __name__ == "__main__":
+    main()
