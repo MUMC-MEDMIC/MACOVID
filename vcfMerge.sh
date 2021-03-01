adec611b95d4c6f1ef2790a27536bebf21a8db28
Brian van der Veer <brian.vander.veer@mumc.nl>
2021-03-01 16:25:16 +0100

    bug fix

--- vcfMerge.sh
+++ vcfMerge.sh
@@ -8,5 +8,5 @@ prefix=$6
 cd $outDir
 artic_vcf_merge $prefix ${curDir}/${scheme} nCoV-2019_1:${pool1Vcf} nCoV-2019_2:${pool2Vcf}
 cd $curDir
-bgzip -f ${prefix}.merged.vcf
-tabix -p vcf ${prefix}.merged.vcf.gz
+bgzip -f ${outDir}${prefix}.merged.vcf
+tabix -p vcf ${outDir}${prefix}.merged.vcf.gz
